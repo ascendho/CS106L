6fa8eb6b4163afad97d83e9f0b42749b4e526272
ascendho <coderhs@foxmail.com>
2025-09-13 11:52:53 +0800

    输入流

---
+++ 05 - Streams/Code/cinGetlineBug.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void cinGetlineBug() {
+  double pi;
+  double tao;
+  std::string name;
+  std::cin >> pi;
+  std::getline(std::cin, name);
+  std::cin >> tao;
+  std::cout << "my name is : " << name << " tao is : " << tao
+            << " pi is : " << pi << '\n';
+}
+
+int main() {
+    cinGetlineBug();
+    return 0;
+}
