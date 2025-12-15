c58193bc1ffc8acc29ccb206b76057164f21ada0
ascendho <coderhs@foxmail.com>
2025-12-15 21:30:04 +0800

    上传类章节代码

---
+++ code/07 - Classes/main.cpp
@@ -0,0 +1,20 @@
+// To compile: g++ -std=c++20 main.cpp StudentID.cpp IntVector.cpp -o main
+// To run: ./main
+
+#include <iostream>
+#include "StudentID.h"
+#include "IntVector.h"
+
+void createStudentID()
+{
+    StudentID sid{"Jacob Roberts-Baca", "jtrb", 545};
+    std::cout << "Name: " << sid.getName() << std::endl;
+    std::cout << "Sunet: " << sid.getSunet() << std::endl;
+    std::cout << "ID Number: " << sid.getIdNumber() << std::endl;
+}
+
+int main()
+{
+    createStudentID();
+    return 0;
+}
