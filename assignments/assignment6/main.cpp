1278649abdece32288995332def0a762f0961c24
ascendho <coderhs@foxmail.com>
2026-03-12 09:45:50 +0800

    完成 assignment6

--- assignments/assignment6/main.cpp
+++ assignments/assignment6/main.cpp
@@ -8,7 +8,7 @@
 #include <type_traits>
 #include <vector>
 
-/** STUDENT_TODO: You will need to include a relevant header file here! */
+#include <optional>
 
 #include "autograder/utils.hpp"
 
@@ -52,10 +52,12 @@ public:
    * @param course_title The title of the course to find.
    * @return You will need to figure this out!
    */
-  FillMeIn find_course(std::string course_title)
+  std::optional<Course> find_course(std::string course_title)
   {
-    /* STUDENT_TODO: Implement this method! You will need to change the return
-     * type. */
+    auto it = std::find_if(courses.begin(), courses.end(),
+                           [&](const Course& c) { return c.title == course_title; });
+    if (it != courses.end()) return *it;
+    return std::nullopt;
   }
 
 private:
@@ -81,7 +83,14 @@ main(int argc, char* argv[])
     Please pay special attention to the README here
     ********************************************************/
 
-    std::string output = /* STUDENT_TODO */
+    std::string output = course
+      .transform([](const Course& c) {
+        return "Found course: " + c.title + "," + c.number_of_units + "," + c.quarter;
+      })
+      .or_else([]() -> std::optional<std::string> {
+        return "Course not found.";
+      })
+      .value();
 
     /********************************************************
      DO NOT MODIFY ANYTHING BELOW THIS LINE PLEASE
